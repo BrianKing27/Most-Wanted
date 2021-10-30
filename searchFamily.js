7d4289e59f1a05cdb5fe7bf7193d7e34f2227d97
unknown <briankingDCC@gmail.com>
2021-10-30 19:22:09 -0400

    Saved code for family search function to searchFamily.js file

--- searchFamily.js
+++ searchFamily.js
@@ -1,75 +1,53 @@
-let sibling = [];
+let spouse = [];
+let theSpouse = [];
+let sibling =[];
 let siblings =[];
-let broAndSis =[];
+
+
 function searchBySiblings(person, people){
   let arrayOfSiblings = people.filter(function (potentialMatch) {
-    if((potentialMatch.parents[0] == person.parents[0] || potentialMatch.parents[1] == person.parents[1]) && potentialMatch !== person && potentialMatch.parents.length >= 0){               
+    if((potentialMatch.parents[0] == person.parents[0] || potentialMatch.parents[1] == person.parents[1]) && potentialMatch !== person && potentialMatch.parents.length > 0 && person.parents.length > 0){               
       return true;
     }   
     else {
       return false;
     }
   });
-    for(let i = 0; i < arrayOfSiblings.length; i++){
-      sibling +=  'Sibling of ' + person.firstName + ' ' + person.lastName + '\n';
-      sibling += '-----------------------' + '\n';
-      sibling += 'First name: ' + arrayOfSiblings[i].firstName + ' ' + '\n';
-      sibling += 'Last name: ' + arrayOfSiblings[i].lastName + '\n';
-      sibling += '\n'
-      siblings = arrayOfSiblings[i];
-      broAndSis = sibling;
-    }
-  alert(broAndSis);
+   for(let i = 0; i < arrayOfSiblings.length; i++){
+    sibling +=  'Sibling of ' + person.firstName + ' ' + person.lastName + '\n';
+    sibling += '-----------------------' + '\n';
+    sibling += 'First name: ' + arrayOfSiblings[i].firstName + ' ' + '\n';
+    sibling += 'Last name: ' + arrayOfSiblings[i].lastName + '\n';
+    sibling += '\n'
+
+    siblings = sibling;
+ }
+ alert(siblings);
+ sibling = [];
+ siblings = [];
 }
 
-let sibling = [];
-let siblings =[];
-let broAndSis =[];
-function searchBySiblings(person, people){
-  let arrayOfSiblings = people.filter(function (potentialMatch) {
-    if((potentialMatch.parents[0] == person.parents[0] || potentialMatch.parents[1] == person.parents[1]) && potentialMatch !== person && potentialMatch.parents.length >= 0){               
+
+function searchBySpouse(person, people){
+  let spouseName = people.filter(function (potentialMatch) {
+    if((potentialMatch.currentSpouse == person.id)){               
       return true;
     }   
     else {
       return false;
     }
   });
-    for(let i = 0; i < arrayOfSiblings.length; i++){
-      sibling +=  'Sibling of ' + person.firstName + ' ' + person.lastName + '\n';
-      sibling += '-----------------------' + '\n';
-      sibling += 'First name: ' + arrayOfSiblings[i].firstName + ' ' + '\n';
-      sibling += 'Last name: ' + arrayOfSiblings[i].lastName + '\n';
-      sibling += '\n'
-      siblings = arrayOfSiblings[i];
-      broAndSis = sibling;
+    for(let i = 0; i < spouseName.length; i++){
+      spouse =  'Spouse of ' + person.firstName + ' ' + person.lastName + '\n';
+      spouse += '-----------------------' + '\n';
+      spouse += 'First name: ' + spouseName[i].firstName + ' ' + '\n';
+      spouse += 'Last name: ' + spouseName[i].lastName + '\n';
+      spouse += '\n';
+      theSpouse = spouse;
+
     }
-  alert(broAndSis);
-}
+    alert(theSpouse);
+    spouse = [];
+    theSpouse = [];
 
-
-
-
-function searchGrandchildren(){
-  switch(grandChildren) {
-    case 'yes' :
-      searchByParents(descendants, data)
-      break;
-    case 'Yes' :
-      searchByParents(descendants, data)
-      break;
-    case 'end' || 'End' || 'stop' || 'Stop' || 'no' || 'No' :
-      return  
-    case 'reset' || 'restart' || 'Reset' || 'Restart' :
-      child =[];
-      children = [];
-      descendants = [];
-      grandChildren = [];
-      app(people);
-      break;
-  }
-}
-child =[];
-children = [];
-descendants = [];
-grandChildren = [];
 }
